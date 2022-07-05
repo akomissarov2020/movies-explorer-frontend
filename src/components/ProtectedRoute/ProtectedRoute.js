cb583aea82bee1b1f3a2ca6acc64f4d2891bb8cb
Aleksey Komissarov <ad3002@gmail.com>
2022-07-05 05:03:56 +0300

    fix profile values

--- src/components/ProtectedRoute/ProtectedRoute.js
+++ src/components/ProtectedRoute/ProtectedRoute.js
@@ -4,7 +4,6 @@ import { Navigate } from "react-router-dom";
 
 const ProtectedRoute = ({children}) => {
     const isLoggedIn = localStorage.getItem("isLoggedIn");
-    console.log("isLoggedIna", isLoggedIn)
     if (!isLoggedIn) {
       return <Navigate to="/" replace />;
     }
